2b9a4096a73d4bdcfdb3298b8fa182bc10152efd
NAWABX007 <harendrasingh55556@gmail.com>
2026-05-25 21:07:09 +0530

    Update bot token and default chat ID in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const parsedId = Number(process.env.ID);
 module.exports = {
-  token: process.env.TOKEN || 'YOUR_BOT_TOKEN_HERE',
-  id: isNaN(parsedId) ? 123456789 : parsedId // replace 12345.. with your telegram chat id
+  token: process.env.TOKEN || '8845234602:AAHBj2FZppzFugiTkhFfwOB8lgPhd5-NV3c',
+  id: isNaN(parsedId) ? 6595438828 : parsedId // replace 12345.. with your telegram chat id
 };
